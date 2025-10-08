6d9ccc47a1f4411516638c5e9e285ab3e48c68ef
nicolastradatti <nicolastradatti@gmail.com>
2025-10-07 23:36:18 -0300

    Create nicolastradatti.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/nicolastradatti.py
@@ -0,0 +1,25 @@
+#https://python.org
+
+"comentario en una linea"
+"""
+comentario
+en varias lineas
+"""
+
+my_variable = "mi variable"
+
+MY_CONSTANT = "mi constante"
+
+my_int = 1
+"int es para enteros"
+
+my_float = 2.5
+"float es para decimales"
+
+my_bool = True
+"bool es para booleanos(True o False)"
+
+my_string = "mi cadena de texto"
+"string es para cadenas de texto"
+
+print("hola,python")
